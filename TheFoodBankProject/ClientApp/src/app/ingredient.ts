9ac830a427b4011b2163e857e50a7c87be38598b
sean <spcavanaugh88@gmail.com>
2021-06-11 15:33:46 -0400

    added some logic

---
+++ TheFoodBankProject/ClientApp/src/app/ingredient.ts
@@ -0,0 +1,6 @@
+export interface Ingredient {
+  id: number;
+  apiId: number;
+  foodName: string;
+  foodImages: string;
+}
--- TheFoodBankProject/ClientApp/src/app/login/login.component.ts
+++ TheFoodBankProject/ClientApp/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { InventoryService } from '../inventory.service';
 
 @Component({
     selector: 'app-login',
@@ -7,8 +9,23 @@
 })
 /** Login component*/
 export class LoginComponent {
+    router: any;
     /** Login ctor */
-    constructor() {
+    constructor(private inventoryService:InventoryService) {
 
-    }
+  }
+
+  myLogin: string = "";
+
+  getLogin(): void {
+    this.myLogin = this.inventoryService.getLogin();
+  }
+
+
+  setLogin(form: NgForm): void {
+    this.inventoryService.setLogin(form.form.value.user);
+    this.getLogin();
+    this.router.navigate(['menu']);
+  }
+
 }
